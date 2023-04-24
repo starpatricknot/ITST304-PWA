fcb966d985df98594ca52de3d50b913012f386e3
Rizaldy Cosico <99704267+starpatricknot@users.noreply.github.com>
2023-04-24 09:44:59 +0800

    semi added a pwa function

--- js/main.js
+++ js/main.js
@@ -22,30 +22,4 @@ document.getElementById("guess-button").addEventListener("click", function() {
   }
 });
 
-var cacheName = 'itst-pwa';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/main.js'
-];
-
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
-  self.skipWaiting();
-});
-
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
-});
 
